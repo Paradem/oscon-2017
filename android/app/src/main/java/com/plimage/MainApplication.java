f292e2dd590782dd015e3f25a158a4e90d1dd3cf
Jon Lebensold <jon@lebensold.ca>
2017-04-26 22:19:29 -0400

    running upgrade

--- android/app/src/main/java/com/plimage/MainApplication.java
+++ android/app/src/main/java/com/plimage/MainApplication.java
@@ -1,11 +1,9 @@
 package com.plimage;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
