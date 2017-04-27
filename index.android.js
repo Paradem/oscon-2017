d1181def0f2158130941e1a71b8c8066a63d3a91
Jon Lebensold <jon@lebensold.ca>
2017-04-27 17:00:07 -0400

    all the android

--- index.android.js
+++ index.android.js
@@ -1,53 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-
+/* global __DEV__ */
 import React from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View,
 } from 'react-native';
+import { connect, Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createLogger from 'redux-logger';
+import {
+  addNavigationHelpers,
+} from 'react-navigation';
+import reducer from './reducers';
+import { AppNavigator } from './reducers/navigation';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+// middleware that logs actions
+const loggerMiddleware = createLogger({ predicate: () => __DEV__ });
 
-const Plimage = () =>
-      (
-        <View style={styles.container}>
-          <Text style={styles.welcome}>
-            Welcome to React Native!
-          </Text>
-          <Text style={styles.instructions}>
-            To get started, edit index.android.js
-          </Text>
-          <Text style={styles.instructions}>
-            Double tap R on your keyboard to reload,{'\n'}
-            Shake or press menu button for dev menu
-          </Text>
-        </View>
-      );
+function configureStore(initialState) {
+  const enhancer = compose(
+    applyMiddleware(
+      loggerMiddleware,
+    ),
+  );
+  return createStore(reducer, initialState, enhancer);
+}
 
-export default Plimage;
+const AppWithNavigationState = connect(state => ({
+  nav: state.nav,
+}))(({ dispatch, nav }) => <AppNavigator navigation={addNavigationHelpers({ dispatch, state: nav })} />);
 
 
-AppRegistry.registerComponent('Plimage', () => Plimage);
+const App = () => (
+  <Provider store={configureStore({})}>
+    <AppWithNavigationState />
+  </Provider>
+);
+
+AppRegistry.registerComponent('Plimage', () => App);
